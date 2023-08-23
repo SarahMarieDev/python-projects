2442d4959e9b232d3c78dafe0413a7c1a5e22626
Schlueter <t462019@deluxe.com>
2023-08-23 12:44:28 -0400

    add functionality to save high score after program exit

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -5,12 +5,13 @@ class Scoreboard(Turtle):
     
     ALIGNMENT = "center"
     FONT = ('Courier', 24, 'normal')
-
+    HIGH_SCORE = open("data.txt", mode="r").read()
+    
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(self.HIGH_SCORE)
         self.penup()
         self.pencolor("white")
         self.hideturtle()
@@ -22,6 +23,7 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=self.ALIGNMENT, font=self.FONT)
         
     def reset(self):
+        self.HIGH_SCORE = open("data.txt", mode="w").write(str(self.high_score))
         if self.score > self.high_score:
             self.high_score = self.score
         self.score = 0
