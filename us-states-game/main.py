bae314660c359e6e8fd35da6ca6cbc1967d3a0a4
Sarah Schlueter <sarahmarie73@gmail.com>
2024-01-05 14:40:03 -0600

    for US States Game: convert guess to title case, check if the guess is in the states data, write correct guesses to the map

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -8,17 +8,32 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another stata's name?")
-
-state_data = pandas.read_csv("50_states.csv")
-current_state = state_data[state_data.state == answer_state]
-
-print(current_state.x)
-
 state_label = turtle.Turtle()
 state_label.hideturtle()
 state_label.penup()
-state_label.write(answer_state, align='center', font=('Arial', 8, 'bold'))
-state_label.position()
+
+
+answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name?").title()
+
+state_data = pandas.read_csv("50_states.csv")
+
+states = state_data["state"].to_list()
+current_state = state_data[state_data["state"] == answer_state]
+x_cor = int(current_state.x.iloc[0])
+y_cor = int(current_state.y.iloc[0])
+
+if answer_state in states:
+    state_label.setpos(x_cor, y_cor)
+    state_label.write(answer_state, align='center', font=('Arial', 8, 'bold'))
+
+else:
+    print("Incorrect")
+
+# TODO: Use a loop to allow the user to keep guessing
+
+# TODO: Record the correct guesses in a list
+
+# TODO: Keep track of the score
+
 
 turtle.mainloop()
