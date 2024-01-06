0f32b5d00c9948bd9877ffec78e5a3e055ed12ce
Schlueter <t462019@deluxe.com>
2024-01-06 00:31:29 -0500

    added some error handling and did some code refactoring

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,5 +1,7 @@
 import turtle
 import pandas
+from tkinter import *
+from tkinter import messagebox
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -12,28 +14,36 @@ state_label = turtle.Turtle()
 state_label.hideturtle()
 state_label.penup()
 
+game_over = False
 
-answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name?").title()
+while not game_over:
+    answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name?")
 
-state_data = pandas.read_csv("50_states.csv")
+    if answer_state == None:
+        game_over = True
+        break
+        
+    answer_state = answer_state.title()
 
-states = state_data["state"].to_list()
-current_state = state_data[state_data["state"] == answer_state]
-x_cor = int(current_state.x.iloc[0])
-y_cor = int(current_state.y.iloc[0])
+    state_data = pandas.read_csv("50_states.csv")
 
-if answer_state in states:
-    state_label.setpos(x_cor, y_cor)
-    state_label.write(answer_state, align='center', font=('Arial', 8, 'bold'))
+    states = state_data["state"].to_list()
+    current_state = state_data[state_data["state"] == answer_state]
 
-else:
-    print("Incorrect")
+    if answer_state in states:
+        x_cor = int(current_state.x.iloc[0])
+        y_cor = int(current_state.y.iloc[0])
+        state_label.setpos(x_cor, y_cor)
+        state_label.write(answer_state, align='center', font=('Arial', 8, 'bold'))
 
-# TODO: Use a loop to allow the user to keep guessing
+    else:
+        messagebox.showerror(title="Incorrect", message="That is not a state. Try again.")
 
-# TODO: Record the correct guesses in a list
+    
 
-# TODO: Keep track of the score
+        # TODO: Record the correct guesses in a list
+
+        # TODO: Keep track of the score
 
 
-turtle.mainloop()
+    turtle.mainloop()
