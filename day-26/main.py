2cda7b01462172d33c99ab63c0b82266cf189d0f
Sarah Schlueter <sarahmarie73@gmail.com>
2024-01-27 15:56:49 -0600

    finish day 26 practice challenges

--- day-26/main.py
+++ day-26/main.py
@@ -1,10 +1,50 @@
 # from coding exercise: Data Overlap
-with open("file1.txt") as file1:
-    list1 = file1.readlines()
+# with open("file1.txt") as file1:
+#     list1 = file1.readlines()
+#
+# with open("file2.txt") as file2:
+#     list2 = file2.readlines()
+#
+# result = [int(num) for num in list1 if num in list2]
+#
+# print(result)
 
-with open("file2.txt") as file2:
-    list2 = file2.readlines()
+# coding exercise: Dictionary Comprehension 1
+# Use Dictionary Comprehension to create a dictionary
+# that takes each word in the given sentence and calculates
+# the number of letters in each word.
+# sentence = input()
+# result = {word: len(word) for word in sentence.split()}
+# print(result)
 
-result = [int(num) for num in list1 if num in list2]
+# coding exercise: Dictionary Comprehension 2
+# Use Dictionary Comprehension to create a dictionary
+# called weather_f that takes each temperature in degrees
+# Celsius and converts it into degrees Fahrenheit.
+# weather_c = eval(input())
+# weather_f = {day:(temp * 9/5) + 32 for (day, temp) in weather_c.items()}
+# print(weather_f)
 
-print(result)
+# Iterate over a Pandas dataframe
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+# Loop through a data frame
+# for (key, value) in student_data_frame.items():
+#     print(value)
+
+# Loop through rows of a dataframe
+for (index, row) in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
