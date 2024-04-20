34b6189b4071db65a31cf268d2923646475b77c5
Sarah Schlueter <sarahmarie73@gmail.com>
2024-04-20 13:02:07 -0500

    create countdown function and tie to start button

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -10,16 +10,31 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # TIMER RESET
 
 # TIMER MECHANISM
+def start_timer():
+    count_down(WORK_MIN)
+
 
 # COUNTDOWN MECHANISM
+def count_down(count):
+    canvas.itemconfig(timer, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # UI SETUP
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=LTGREEN)
+
+canvas = Canvas(width=200, height=224, bg=LTGREEN, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
 timer_label = Label(text="Timer", font=(FONT_NAME, 55), fg=DKGREEN, bg=LTGREEN)
 timer_label.grid(column=1, row=0)
@@ -29,17 +44,10 @@ checkmark_label = Label(text="✔", fg=DKGREEN, bg=LTGREEN)
 checkmark_label.grid(column=1, row=3)
 checkmark_label.config(pady=10)
 
-start_button = Button(text="Start", highlightbackground=LTGREEN)
+start_button = Button(text="Start", highlightbackground=LTGREEN, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightbackground=LTGREEN)
 reset_button.grid(column=2, row=2)
-
-canvas = Canvas(width=200, height=224, bg=LTGREEN, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(column=1, row=1)
-
 
 window.mainloop()
