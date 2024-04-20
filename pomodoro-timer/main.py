9359ed00b53f17e97b06576783d04b829276bd0c
Sarah Schlueter <sarahmarie73@gmail.com>
2024-04-20 14:23:52 -0500

    remove test code

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -20,9 +20,9 @@ def start_timer():
     global reps
     reps += 1
 
-    work_sec = 2#WORK_MIN * 60
-    short_break_sec = 2#SHORT_BREAK_MIN * 60
-    long_break_sec = 2#LONG_BREAK_MIN * 60
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         count_down(long_break_sec)
