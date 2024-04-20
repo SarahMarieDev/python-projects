2c552f4a3de9f23d2018af090814aef1b54457b8
Sarah Schlueter <sarahmarie73@gmail.com>
2024-04-20 13:59:51 -0500

    change color scheme

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -2,10 +2,10 @@ from tkinter import *
 import math
 
 # CONSTANTS
-BLUE = "#496989"
-DKGREEN = "#58A399"
-GREEN = "#A8CD9F"
-LTGREEN = "#E2F4C5"
+ORANGE = "#FAA300"
+PINK = "#F4538A"
+BLUE = "#59D5E0"
+BEIGE = "#FFF8DC"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -18,19 +18,22 @@ reps = 0
 # TIMER MECHANISM
 def start_timer():
     global reps
+    reps += 1
+
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    if reps % 2 == 0:
-        count_down(work_sec)
-        reps += 1
-    elif reps % 2 == 1 and reps <= 5:
-        count_down(short_break_sec)
-        reps += 1
-    else:
+    if reps % 8 == 0:
         count_down(long_break_sec)
-        reps = 0
+        timer_label.config(text="Break", fg=PINK)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=ORANGE)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=BLUE)
+
 
 # COUNTDOWN MECHANISM
 def count_down(count):
@@ -46,26 +49,25 @@ def count_down(count):
 # UI SETUP
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=LTGREEN)
+window.config(padx=100, pady=50, bg=BEIGE)
 
-canvas = Canvas(width=200, height=224, bg=LTGREEN, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=BEIGE, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 55), fg=DKGREEN, bg=LTGREEN)
+timer_label = Label(text="Timer", font=(FONT_NAME, 55), fg="black", bg=BEIGE)
 timer_label.grid(column=1, row=0)
 timer_label.config(pady=10)
 
-checkmark_label = Label(text="✔", fg=DKGREEN, bg=LTGREEN)
+checkmark_label = Label(fg=BLUE, bg=BEIGE)
 checkmark_label.grid(column=1, row=3)
-checkmark_label.config(pady=10)
 
-start_button = Button(text="Start", highlightbackground=LTGREEN, command=start_timer)
+start_button = Button(text="Start", highlightbackground=BEIGE, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightbackground=LTGREEN)
+reset_button = Button(text="Reset", highlightbackground=BEIGE)
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
