ef2c562dabb1763a9f67dcbb52381390b622d21f
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-21 11:32:06 -0500

    create field labels

--- password-manager/main.py
+++ password-manager/main.py
@@ -11,7 +11,16 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+login_label = Label(text="Email/Username:")
+login_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
 
 
 window.mainloop()
