689d051a52464f9e14f7b9936038866cfcf05dee
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-21 12:17:07 -0500

    finish UI setup - add entries and buttons, fix layout

--- password-manager/main.py
+++ password-manager/main.py
@@ -6,21 +6,32 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo)
+canvas.create_image(130, 100, image=logo)
 canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2, sticky="nsew")
 
 login_label = Label(text="Email/Username:")
 login_label.grid(column=0, row=2)
+login_entry = Entry(width=35)
+login_entry.grid(column=1, row=2, columnspan=2, sticky="nsew")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3, sticky="nsew")
 
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3, sticky="nsew")
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2, sticky="nsew")
 
 window.mainloop()
