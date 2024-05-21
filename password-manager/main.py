0033a31392c49ebbbde58de554b6524bbab9317d
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-21 18:32:52 -0500

    add password generation code

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,6 +1,30 @@
+from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import simpledialog
 from tkinter import messagebox
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    pw_letters = [choice(letters) for _ in range(randint(8, 10))]
+    pw_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    pw_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = pw_letters + pw_symbols + pw_numbers
+
+    shuffle(password_list)
+
+    generated_password = "".join(password_list)
+
+    print(f"Your password is: {generated_password}")
+
 
 
 def custom_askokcancel(title, message):
