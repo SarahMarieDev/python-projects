5cb89ed984a89d4216446b064a59898c8c48ab47
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-23 15:47:33 -0500

    fix interpreter settings

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -3,6 +3,7 @@ import pandas
 nato_data_frame = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
 
+
 def generate_phonetic():
     input_string = input("Enter a word: ")
     try:
@@ -12,5 +13,6 @@ def generate_phonetic():
         generate_phonetic()
     else:
         print(output_list)
+
 
 generate_phonetic()
