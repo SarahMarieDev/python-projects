3d622425a5c893cfcc3340f736fda9ab8fc686ca
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-23 16:44:32 -0500

    add exception handling

--- password-manager/main.py
+++ password-manager/main.py
@@ -42,16 +42,20 @@ def save_password():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        with open("../../../Documents/Data.json", "r") as password_file:
-            data = json.load(password_file)
+        try:
+            with open("../../../Documents/Data.json", "r") as password_file:
+                data = json.load(password_file)
+        except FileNotFoundError:
+            with open("../../../Documents/Data.json", "w") as password_file:
+                json.dump(new_data, password_file, indent=4)
+        else:
             data.update(new_data)
-
-        with open("../../../Documents/Data.json", "w") as password_file:
-            json.dump(data, password_file, indent=4)
-
+            with open("../../../Documents/Data.json", "w") as password_file:
+                json.dump(data, password_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
-            messagebox.showinfo(title="Success", message=f"Login information for {website} saved successfully.")
+    messagebox.showinfo(title="Success", message=f"Login information for {website} saved successfully.")
 
 
 window = Tk()
