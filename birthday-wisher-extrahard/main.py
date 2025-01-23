1c70af50861197f86994fd3296a4f4037896859a
Sarah Schlueter <sarahmarie73@gmail.com>
2025-01-23 14:44:16 -0600

    finish automated birthday wisher project

--- birthday-wisher-extrahard/main.py
+++ birthday-wisher-extrahard/main.py
@@ -1,12 +1,3 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
 import os
 import smtplib
 import pandas
@@ -16,19 +7,19 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-def send_email(recipient_email, letter):
-    my_email = "sarah_schlueter73@yahoo.com"
-    password = "vnlj qern quhq zzhy"
+def send_email(recipient_email, message):
+    my_email = "sarahschlueter99@gmail.com"
+    password = "jeuykbiqygmdjtyb"
 
     msg = MIMEMultipart()
     msg['From'] = my_email
     msg['To'] = recipient_email
     msg['Subject'] = "Happy Birthday!"
 
-    body = letter
+    body = message
     msg.attach(MIMEText(body, 'plain'))
 
-    with smtplib.SMTP("smtp.mail.yahoo.com", 587) as connection:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(
@@ -43,5 +34,8 @@ letter_template = random.choice([x for x in os.listdir("letter_templates")])
 
 csv_data = pandas.read_csv("birthdays.csv")
 for index, row in csv_data.iterrows():
-     if (row.month, row.day) == today:
-         print(row)
+      if (row.month, row.day) == today:
+        first_name = row['name'].split()[0]   
+        with open(f"letter_templates/{letter_template}") as file:
+            message = file.read().replace("[NAME]", first_name)
+            send_email(row.email, message)
