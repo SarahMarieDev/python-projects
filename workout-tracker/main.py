0937bd8e0ba5fccfa14f354a9825dcf3b988f4e3
Sarah Schlueter <sarahmarie73@gmail.com>
2025-04-22 20:59:26 -0500

    parse workout data and add rows to sheet for each entry

--- workout-tracker/main.py
+++ workout-tracker/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 from datetime import *
 
@@ -11,7 +12,7 @@ API_KEY = os.getenv('NUTRITIONIX_API_KEY')
 base_url = "https://trackapi.nutritionix.com"
 exercise_endpoint = "/v2/natural/exercise"
 
-user_input = input("Tell me which exercises you did: ")
+#user_input = input("Tell me which exercises you did: ")
 
 headers = {
     "x-app-id": APP_ID,
@@ -19,9 +20,42 @@ headers = {
 }
 
 request_body = {
-    "query": user_input
+    "query": "I ran 5k and cycled 30 min"
 }
 
 
 response = requests.post(url=f"{base_url}{exercise_endpoint}", json=request_body, headers=headers)
-print(response.text)
+
+exercise_data = json.loads(response.text)
+print(json.dumps(exercise_data, indent=2))
+
+sheety_url = "https://api.sheety.co/93ee438cd8c9e99f55862b9897a19e34/workoutTracking/workouts"
+
+sheety_headers = {
+    "Content-Type": "application/json"
+}
+
+current_date = datetime.now().strftime("%m/%d/%Y")
+current_time = datetime.now().strftime("%X")
+
+for exercise in exercise_data["exercises"]:
+    exercise_name = f"{exercise['name']}".title()
+    duration = f"{exercise['duration_min']}"
+    calories = f"{exercise['nf_calories']}"
+    
+    row_params = {
+        "workout": {
+            "date": current_date,
+            "time": current_time,
+            "exercise": exercise_name,
+            "duration": duration,
+            "calories": calories
+        }
+    }
+    
+    response = requests.post(url=f"{sheety_url}", json=row_params, headers=sheety_headers)
+    print(response.text)
+    
+    
+
+
