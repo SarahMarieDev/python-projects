7fd97a19a7cb258dac70734629d69fd182aabf86
Sarah Schlueter <sarahmarie73@gmail.com>
2025-07-19 14:34:48 -0500

    add token to env

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -6,17 +6,15 @@ from dotenv import load_dotenv
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     load_dotenv()
-
-    USERNAME = os.getenv('FD_USER')
-    PASSWORD = os.getenv('FD_PASS')
     
     def __init__(self):
+        TOKEN = os.getenv('FD_AUTH')
         self.base_url = (
             "https://api.sheety.co/93ee438cd8c9e99f55862b9897a19e34/flightDeals/prices"
         )
         self.headers = {
             "Content-Type": "application/json",
-            "Authorization": "Basic c2FyYWhtYXJpZWRldjpMZXhpJk5hbm8xOTg2"
+            "Authorization": TOKEN
         }
         
     def get_prices(self):
