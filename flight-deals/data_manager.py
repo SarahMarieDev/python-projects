3ea35f42c16c347e0684d14fc7a2b72656006aeb
Sarah Schlueter <sarahmarie73@gmail.com>
2025-07-18 21:06:54 -0500

    change auth and load env variables

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -1,14 +1,22 @@
+import os
 import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
+    load_dotenv()
+
+    USERNAME = os.getenv('FD_USER')
+    PASSWORD = os.getenv('FD_PASS')
     
     def __init__(self):
         self.base_url = (
             "https://api.sheety.co/93ee438cd8c9e99f55862b9897a19e34/flightDeals/prices"
         )
         self.headers = {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Basic c2FyYWhtYXJpZWRldjpMZXhpJk5hbm8xOTg2"
         }
         
     def get_prices(self):
